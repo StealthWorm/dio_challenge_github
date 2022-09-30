44108da60d4210c39d1196dd975b082dd55fa220
StealthWorm <thierrypitela@hotmail.com>
2022-09-29 21:47:50 -0300

    Mini calculator config + debug tests

---
+++ Dominando a Linguagem de Programação Java/Projetos/projeto1/src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,45 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+
+		int a, b;
+
+		System.out.println("Digite o primeiro valor para realizar a operação");
+		a = scan.nextInt();
+		System.out.println("Digite o primeiro valor para realizar a operação");
+		b = scan.nextInt();
+
+		int soma = soma(a, b);
+		int subtracao = subtracao(a, b);
+		double divisao = divisao(a, b);
+		int multiplicacao = multiplicacao(a, b);
+
+		System.out.println("soma: " + soma);
+		System.out.println("subtracao: " + subtracao);
+		System.out.println("divisao: " + String.format("%.2f", divisao));
+		System.out.println("multiplicacao: " + multiplicacao);
+
+	}
+
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+
+	public static int divisao(int a, int b) {
+		return a / b;
+	}
+
+	public static int multiplicacao(int a, int b) {
+		return a * b;
+	}
+
+}
