74605d04d709c3711bbdd0444b326b79496af208
StealthWorm <thierrypitela@hotmail.com>
2022-09-30 21:03:33 -0300

    Variables code sample Java

---
+++ Dominando a Linguagem de Programação Java/Projetos/variaveis/src/br/com/dio/Variaveis.java
@@ -0,0 +1,24 @@
+package br.com.dio;
+
+public class Variaveis {
+    public static void main(String[] args) {
+
+        int numeroTentativas;
+        int I;
+        int _1a;
+        int $aq;
+
+        numeroTentativas = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int NUMERO_TENTATIVAS = 10; //valores final nao podem ser alterados
+
+        System.out.println(numeroTentativas);
+        System.out.println(I);
+        System.out.println(_1a);
+        System.out.println($aq);
+        System.out.println(NUMERO_TENTATIVAS);
+    }
+}
