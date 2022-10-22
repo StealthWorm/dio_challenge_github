9591c28ce7cfa6574691af22868eff3a3b418f1c
StealthWorm <thierrypitela@hotmail.com>
2022-10-21 23:21:46 -0300

    feat: Sprin Boot Projects + tests + Spring Framework principles

---
+++ Avançando Tecnicamente com Spring Framework e API/Projetos/first-proj-spring/src/main/java/br/com/firstProjSpring/firstprojspring/FirstProjSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.firstProjSpring.firstprojspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstProjSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstProjSpringApplication.class, args);
+	}
+
+}
