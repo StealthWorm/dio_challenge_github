f10e54a0c205ca8da78ae657d655bafc68b45df4
StealthWorm <thierrypitela@hotmail.com>
2022-10-22 07:59:07 -0300

    feat: dependency injection test

---
+++ Avançando Tecnicamente com Spring Framework e API/Projetos/DependencyInjection/src/main/java/org/example/Carro.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Carro implements IVeiculo {
+
+    @Override
+    public void acao() {
+        System.out.println("É um carro.");
+    }
+}
---
+++ Avançando Tecnicamente com Spring Framework e API/Projetos/DependencyInjection/src/main/java/org/example/Moto.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Moto implements IVeiculo {
+
+    @Override
+    public void acao() {
+        System.out.println("É uma moto.");
+    }
+}
